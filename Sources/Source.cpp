0a4fbe524e1a06b778696104c68d4a44916f30c3
Daria <tokdariatok@gmail.com>
2019-05-26 19:43:20 +0300

    add hero to screen

--- Sources/Source.cpp
+++ Sources/Source.cpp
@@ -30,6 +30,19 @@ int main()
 	quitActiveButton.sprite.setPosition(700, 650);
 
 	RenderWindow window(VideoMode(1200, 800), "Bomberman");
+
+	//Для добавления персонажа
+	Texture herotexture;
+	herotexture.loadFromFile("Data/heroBomb.png");
+
+	Sprite herosprite;
+	herosprite.setTexture(herotexture);
+	herosprite.setTextureRect(IntRect(0, 0, 50, 50));
+	herosprite.setPosition(250, 250);
+
+	float CurrentFrame = 0;//хранит текущий кадр
+	Clock clock;
+
 	while (window.isOpen())
 	{
 		//Для хранения координат мыши
@@ -106,10 +119,56 @@ int main()
 			
 			if (levelInProcess)
 			{
+				
 				// there will be action
+				window.clear();
+				
+				float time = clock.getElapsedTime().asMicroseconds();
+				clock.restart();
+				time = time / 800;
+
+				//Управление персонажем с анимацией
+				
+				if ((Keyboard::isKeyPressed(Keyboard::Left) || (Keyboard::isKeyPressed(Keyboard::A)))) 
+				{ 
+					CurrentFrame += 0.005*time; //служит для прохождения по кадрам
+					if (CurrentFrame > 5) CurrentFrame -= 5; 
+					herosprite.setTextureRect(IntRect(50 * int(CurrentFrame), 50, 50, 50)); 
+					herosprite.move(-0.1*time, 0);
+				}
+
+				if ((Keyboard::isKeyPressed(Keyboard::Right) || (Keyboard::isKeyPressed(Keyboard::D)))) 
+				{
+					CurrentFrame += 0.005*time; 
+					if (CurrentFrame > 5) CurrentFrame -= 5; 
+					herosprite.setTextureRect(IntRect(50 * int(CurrentFrame), 0, 50, 50)); 
+					herosprite.move(0.1*time, 0);
+
+				}
+
+				if ((Keyboard::isKeyPressed(Keyboard::Up) || (Keyboard::isKeyPressed(Keyboard::W)))) 
+				{
+					CurrentFrame += 0.005*time; 
+					if (CurrentFrame > 3) CurrentFrame -= 3; 
+					herosprite.setTextureRect(IntRect(50 * int(CurrentFrame), 150, 50, 50)); 
+					herosprite.move(0, -0.1*time);
+				}
+
+				if ((Keyboard::isKeyPressed(Keyboard::Down) || (Keyboard::isKeyPressed(Keyboard::S)))) 
+				{
+					CurrentFrame += 0.005*time; 
+					if (CurrentFrame > 3) CurrentFrame -= 3; 
+					herosprite.setTextureRect(IntRect(50 * int(CurrentFrame), 100, 50, 50)); 
+					herosprite.move(0, 0.1*time);
+				}
+
+				window.clear();
+				window.draw(herosprite);
 			}
 		}
+		
 		window.display();
+		
 	}
 	return 0;
 }
