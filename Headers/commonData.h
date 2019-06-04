ab27bdc769bdb6cc805070e760c47f640df7ea3b
LaCost765 <lacost765123@gmail.com>
2019-06-04 18:54:00 +0300

    Add normal character control and much more

--- Headers/commonData.h
+++ Headers/commonData.h
@@ -1,58 +1,25 @@
-#pragma once
-class Picture
-{
-public:
-	float x, y, width, height; //x,y - координаты точки на картинке, откуда она будет срисовываться, width, height - ширина и высота
-	String File;																							//срисовываемого участка
-	Sprite sprite;
-	Image image;
-	Texture texture;
-	Picture(String f, float x, float y, float wid, float heig)
-	{
-		this->x = x;
-		this->y = y;
-		this->width = wid;
-		this->height = heig;
-		File = f;
-		image.loadFromFile(File);
-		texture.loadFromImage(image);
-		sprite.setTexture(texture);
-		sprite.setTextureRect(IntRect(this->x, this->y, width, height));
-	}
-};
+Picture bomberMan("Data/bomberman.png", 0, 0, 573, 780, 0, 0);
+Picture titleBomberMan("Data/titleBomberman.png", 0, 0, 600, 150, 600, 50);
+Picture newGameButton("Data/newGameButton.png", 0, 0, 400, 100, 700, 250);
+Picture newGameActiveButton("Data/newGameActiveButton.png", 0, 0, 400, 100, 700, 250);
+Picture continueButton("Data/continueButton.png", 0, 0, 400, 100, 700, 450);
+Picture continueActiveButton("Data/continueActiveButton.png", 0, 0, 400, 100, 700, 450);
+Picture quitButton("Data/quitButton.png", 0, 0, 400, 100, 700, 650);
+Picture quitActiveButton("Data/quitActiveButton.png", 0, 0, 400, 100, 700, 650);
+Picture background("Data/background.jpg", 0, 0, 1600, 800, 0, 0);
+Picture note1("Data/note1.png", 0, 0, 600, 150, 300, 50);
+Picture note2("Data/note2.png", 0, 0, 800, 150, 200, 150);
+Picture yesButton("Data/yesButton.png", 0, 0, 200, 200, 300, 400);
+Picture noButton("Data/noButton.png", 0, 0, 200, 200, 700, 400);
+Picture yesActiveButton("Data/yesActiveButton.png", 0, 0, 200, 200, 300, 400);
+Picture noActiveButton("Data/noActiveButton.png", 0, 0, 200, 200, 700, 400);
+Picture stage1Header("Data/stage1Header.png", 0, 0, 300, 100, 0, 0);
+Picture timeInfoHeader("Data/timeHeader.png", 0, 0, 200, 100, 400, 0);
+Picture health("Data/health.png", 0, 0, 150, 150, 1050, 0);
+Player hero("Data/heroBomb.png", 50, 100, 50.0, 50.0);
+StageMap mainMap;
+View view;
 
-Picture bomberMan("Data/bomberman.png", 0, 0, 573, 780);
-Picture titleBomberMan("Data/titleBomberman.png", 0, 0, 600, 150);
-Picture newGameButton("Data/newGameButton.png", 0, 0, 400, 100);
-Picture newGameActiveButton("Data/newGameActiveButton.png", 0, 0, 400, 100);
-Picture continueButton("Data/continueButton.png", 0, 0, 400, 100);
-Picture continueActiveButton("Data/continueActiveButton.png", 0, 0, 400, 100);
-Picture quitButton("Data/quitButton.png", 0, 0, 400, 100);
-Picture quitActiveButton("Data/quitActiveButton.png", 0, 0, 400, 100);
-Picture background("Data/background.jpg", 0, 0, 1600, 800);
-Picture note1("Data/note1.png", 0, 0, 600, 150);
-Picture note2("Data/note2.png", 0, 0, 800, 150);
-Picture yesButton("Data/yesButton.png", 0, 0, 200, 200);
-Picture noButton("Data/noButton.png", 0, 0, 200, 200);
-Picture yesActiveButton("Data/yesActiveButton.png", 0, 0, 200, 200);
-Picture noActiveButton("Data/noActiveButton.png", 0, 0, 200, 200);
 bool inGame = false;
-
-void setParametrs() {
-	// select position for start menu items
-	yesButton.sprite.setPosition(300, 400);
-	noButton.sprite.setPosition(700, 400);
-	yesActiveButton.sprite.setPosition(300, 400);
-	noActiveButton.sprite.setPosition(700, 400);
-	note1.sprite.setPosition(300, 50);
-	note2.sprite.setPosition(200, 150);
-	background.sprite.setPosition(0, 0);
-	bomberMan.sprite.setPosition(0, 0);
-	titleBomberMan.sprite.setPosition(600, 50);
-	newGameButton.sprite.setPosition(700, 250);
-	continueButton.sprite.setPosition(700, 450);
-	quitButton.sprite.setPosition(700, 650);
-	newGameActiveButton.sprite.setPosition(700, 250);
-	continueActiveButton.sprite.setPosition(700, 450);
-	quitActiveButton.sprite.setPosition(700, 650);
-}
+bool newGameMenu = false;
+bool levelInProcess = false;
--- Sources/EgorFunctions.cpp
+++ Sources/EgorFunctions.cpp
@@ -1,1 +1,210 @@
 //сюда пишем определения функций 
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "Dasha.h"
+#include "Leha.h"
+
+using namespace sf;
+
+void Player::move(float time, float& CurrentFrame, Player& hero, StageMap& mainMap, View& view, Sprite& stage, Sprite& timeInfo, Sprite& health)
+{
+	if ((Keyboard::isKeyPressed(Keyboard::Left) || (Keyboard::isKeyPressed(Keyboard::A)))) {
+
+		if (tileMap[int((this->y - SHIFT) / 50)][int(this->x / 50) - 1] == 'w' || tileMap[int((this->y - SHIFT) / 50)][int(this->x / 50) - 1] == 'b')
+		{
+			if (tileMap[int((this->y - SHIFT) / 50) - 1][int(this->x / 50) - 1] == ' ' || tileMap[int((this->y - SHIFT) / 50) + 1][int(this->x / 50) - 1] == ' ')
+			{
+				if (this->x == int(this->x / 50) * 50)
+				{
+					if (this->y - int(this->y / 50) * 50 >= 25 && tileMap[int((this->y - SHIFT) / 50) + 1][int(this->x / 50) - 1] == ' ')
+					{
+
+						hero.dir = 2; hero.speed = 0.1;
+						CurrentFrame += 0.006*time;
+						if (CurrentFrame > 5) CurrentFrame -= 5;
+						hero.sprite.setTextureRect(IntRect(50 * int(CurrentFrame), 50, 50, 50));
+						mainMap.camera_Follow(hero.x, hero.y, view, stage, timeInfo, health);
+						return;
+					}
+				}
+			}
+		}
+
+		if (tileMap[int((this->y - SHIFT) / 50)][int(this->x / 50) - 1] == ' ' && this->y - int(this->y / 50) * 50 < 25)
+		{
+			if (this->y != int(this->y / 50) * 50 && this->y - int(this->y / 50) * 50 > 1)
+			{
+				hero.dir = 3; hero.speed = 0.1;
+				CurrentFrame += 0.006*time;
+				if (CurrentFrame > 5) CurrentFrame -= 5;
+				hero.sprite.setTextureRect(IntRect(50 * int(CurrentFrame), 50, 50, 50));
+				mainMap.camera_Follow(hero.x, hero.y, view, stage, timeInfo, health);
+				return;
+			}
+			else
+			{
+				this->y = int(this->y / 50) * 50;
+			}
+		}
+
+		hero.dir = 1; hero.speed = 0.1;
+		CurrentFrame += 0.006*time;
+		if (CurrentFrame > 5) CurrentFrame -= 5;
+		hero.sprite.setTextureRect(IntRect(50 * int(CurrentFrame), 50, 50, 50));
+		mainMap.camera_Follow(hero.x, hero.y, view, stage, timeInfo, health);
+	}
+
+	if ((Keyboard::isKeyPressed(Keyboard::Right) || (Keyboard::isKeyPressed(Keyboard::D)))) {
+
+		if (tileMap[int(this->y - SHIFT) / 50][int(this->x / 50) + 1] == 'w' || tileMap[int(this->y - SHIFT) / 50][int(this->x / 50) + 1] == 'b')
+		{
+			if (tileMap[int(this->y - SHIFT) / 50 + 1][int(this->x / 50) + 1] == ' ' || tileMap[int(this->y - SHIFT) / 50 - 1][int(this->x / 50) + 1] == ' ')
+			{
+				if (this->x == int(this->x / 50) * 50)
+				{
+					if (this->y - int(this->y / 50) * 50 >= 25 && tileMap[int((this->y - SHIFT) / 50) + 1][int(this->x / 50) + 1] == ' ')
+					{
+
+						hero.dir = 2; hero.speed = 0.1;
+						CurrentFrame += 0.006*time;
+						if (CurrentFrame > 5) CurrentFrame -= 5;
+						hero.sprite.setTextureRect(IntRect(50 * int(CurrentFrame), 0, 50, 50));
+						mainMap.camera_Follow(hero.x, hero.y, view, stage, timeInfo, health);
+						return;
+					}
+				}
+			}
+		}
+
+		if (tileMap[int((this->y - SHIFT) / 50)][int(this->x / 50) + 1] == ' ' && this->y - int(this->y / 50) * 50 < 25)
+		{
+			if (this->y != int(this->y / 50) * 50 && this->y - int(this->y / 50) * 50 > 1)
+			{
+				hero.dir = 3; hero.speed = 0.1;
+				CurrentFrame += 0.006*time;
+				if (CurrentFrame > 5) CurrentFrame -= 5;
+				hero.sprite.setTextureRect(IntRect(50 * int(CurrentFrame), 0, 50, 50));
+				mainMap.camera_Follow(hero.x, hero.y, view, stage, timeInfo, health);
+				return;
+			}
+			else
+			{
+				this->y = int(this->y / 50) * 50;
+			}
+		}
+
+		hero.dir = 0; hero.speed = 0.1;
+		CurrentFrame += 0.006*time;
+		if (CurrentFrame > 5) CurrentFrame -= 5;
+		hero.sprite.setTextureRect(IntRect(50 * int(CurrentFrame), 0, 50, 50));
+		mainMap.camera_Follow(hero.x, hero.y, view, stage, timeInfo, health);
+	}
+
+	if ((Keyboard::isKeyPressed(Keyboard::Up) || (Keyboard::isKeyPressed(Keyboard::W)))) {
+
+		if (tileMap[int((this->y - SHIFT) / 50) - 1][int(this->x / 50)] == 'w' || tileMap[int((this->y - SHIFT) / 50) - 1][int(this->x / 50)] == 'b')
+		{
+			if (tileMap[int((this->y - SHIFT) / 50) - 1][int(this->x / 50) - 1] == ' ' || tileMap[int((this->y - SHIFT) / 50) - 1][int(this->x / 50) + 1] == ' ')
+			{
+				if (this->y == int(this->y / 50) * 50)
+				{
+					if (this->x - int(this->x / 50) * 50 >= 25 && tileMap[int((this->y - SHIFT) / 50) - 1][int(this->x / 50) + 1] == ' ')
+					{
+
+						hero.dir = 0; hero.speed = 0.1;
+						CurrentFrame += 0.006*time;
+						if (CurrentFrame > 5) CurrentFrame -= 5;
+						hero.sprite.setTextureRect(IntRect(50 * int(CurrentFrame), 0, 50, 50));
+						mainMap.camera_Follow(hero.x, hero.y, view, stage, timeInfo, health);
+						return;
+					}
+				}
+			}
+		}
+
+		if (tileMap[int((this->y - SHIFT) / 50) - 1][int(this->x / 50)] == ' ' && this->x - int(this->x / 50) * 50 < 25)
+		{
+			if (this->x != int(this->x / 50) * 50 && this->x - int(this->x / 50) * 50 > 1)
+			{
+				hero.dir = 1; hero.speed = 0.1;
+				CurrentFrame += 0.006*time;
+				if (CurrentFrame > 5) CurrentFrame -= 5;
+				hero.sprite.setTextureRect(IntRect(50 * int(CurrentFrame), 50, 50, 50));
+				mainMap.camera_Follow(hero.x, hero.y, view, stage, timeInfo, health);
+				return;
+			}
+			else
+			{
+				this->x = int(this->x / 50) * 50;
+			}
+		}
+
+		hero.dir = 3; hero.speed = 0.1;
+		CurrentFrame += 0.006*time;
+		if (CurrentFrame > 3) CurrentFrame -= 3;
+		hero.sprite.setTextureRect(IntRect(50 * int(CurrentFrame), 150, 50, 50));
+	}
+
+	if ((Keyboard::isKeyPressed(Keyboard::Down) || (Keyboard::isKeyPressed(Keyboard::S)))) {
+
+		if (tileMap[int((this->y - SHIFT) / 50) + 1][int(this->x / 50)] == 'w' || tileMap[int((this->y - SHIFT) / 50) + 1][int(this->x / 50)] == 'b')
+		{
+			if (tileMap[int((this->y - SHIFT) / 50) + 1][int(this->x / 50) + 1] == ' ' || tileMap[int((this->y - SHIFT) / 50) + 1][int(this->x / 50) - 1] == ' ')
+			{
+				if (this->y == int(this->y / 50) * 50)
+				{
+					if (this->x - int(this->x / 50) * 50 >= 25 && tileMap[int((this->y - SHIFT) / 50) + 1][int(this->x / 50) + 1] == ' ')
+					{
+
+						hero.dir = 0; hero.speed = 0.1;
+						CurrentFrame += 0.006*time;
+						if (CurrentFrame > 5) CurrentFrame -= 5;
+						hero.sprite.setTextureRect(IntRect(50 * int(CurrentFrame), 0, 50, 50));
+						mainMap.camera_Follow(hero.x, hero.y, view, stage, timeInfo, health);
+						return;
+					}
+				}
+			}
+		}
+
+		if (tileMap[int((this->y - SHIFT) / 50) + 1][int(this->x / 50)] == ' ' && this->x - int(this->x / 50) * 50 < 25)
+		{
+			if (this->x != int(this->x / 50) * 50 && this->x - int(this->x / 50) * 50 > 1)
+			{
+				hero.dir = 1; hero.speed = 0.1;
+				CurrentFrame += 0.006*time;
+				if (CurrentFrame > 5) CurrentFrame -= 5;
+				hero.sprite.setTextureRect(IntRect(50 * int(CurrentFrame), 50, 50, 50));
+				mainMap.camera_Follow(hero.x, hero.y, view, stage, timeInfo, health);
+				return;
+			}
+			else
+			{
+				this->x = int(this->x / 50) * 50;
+			}
+		}
+
+		hero.dir = 2; hero.speed = 0.1;
+		CurrentFrame += 0.006*time;
+		if (CurrentFrame > 3) CurrentFrame -= 3;
+		hero.sprite.setTextureRect(IntRect(50 * int(CurrentFrame), 100, 50, 50));
+	}
+}
+
+void Player::update(float time, Player & hero) // function of "update" the class object.
+{
+	switch (dir)
+	{
+	case 0: dx = speed; dy = 0;   break;
+	case 1: dx = -speed; dy = 0;   break;
+	case 2: dx = 0; dy = speed;   break;
+	case 3: dx = 0; dy = -speed;   break;
+	}
+
+	x += dx * time;
+	y += dy * time;
+
+	speed = 0;
+	sprite.setPosition(x, y);
+	this->block_Motion();
+}
