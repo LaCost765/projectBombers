adc7faec6ea52b52cd6099912984ce1d19ae773e
Mikhail Krivdin <xhazker@gmail.com>
2019-06-06 22:02:26 +0300

    add enemy

--- Headers/Misha.h
+++ Headers/Misha.h
@@ -1,2 +1,20 @@
 #pragma once
-//—юда пишем прототипы функций 
+#include "Leha.h"
+
+using namespace sf;
+
+class Enemy : public Parent
+{
+public:
+	bool dirs[5];
+	
+	Enemy(String F, float X, float Y, float W, float H) : Parent(F, X, Y, W, H)
+	{
+		
+	}
+
+	void check(StageMap& mainMap);
+	void movement(float time, float& CurrentFrame, StageMap& mainMap);
+	void update2(float time, StageMap& mainMap);
+	int chooseDir();
+};
